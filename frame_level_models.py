fecd42fc7321d286328382f8a3cfd6f282bb9734
Strideradu <dunan00001@hotmail.com>
2017-04-22 15:41:54 -0400

    Modify to resolve real conflicts

--- frame_level_models.py
+++ frame_level_models.py
@@ -199,44 +199,42 @@ class DbofModel(models.BaseModel):
 
 class LstmModel(models.BaseModel):
 
-    def create_model(self, model_input, vocab_size, num_frames, **unused_params):
-        """Creates a model which uses a stack of LSTMs to represent the video.
+  def create_model(self, model_input, vocab_size, num_frames, **unused_params):
+    """Creates a model which uses a stack of LSTMs to represent the video.
+    Args:
+      model_input: A 'batch_size' x 'max_frames' x 'num_features' matrix of
+                   input features.
+      vocab_size: The number of classes in the dataset.
+      num_frames: A vector of length 'batch' which indicates the number of
+           frames for each video (before padding).
+    Returns:
+      A dictionary with a tensor containing the probability predictions of the
+      model in the 'predictions' key. The dimensions of the tensor are
+      'batch_size' x 'num_classes'.
+    """
+    lstm_size = FLAGS.lstm_cells
+    number_of_layers = FLAGS.lstm_layers
 
-        Args:
-          model_input: A 'batch_size' x 'max_frames' x 'num_features' matrix of
-                       input features.
-          vocab_size: The number of classes in the dataset.
-          num_frames: A vector of length 'batch' which indicates the number of
-               frames for each video (before padding).
-
-        Returns:
-          A dictionary with a tensor containing the probability predictions of the
-          model in the 'predictions' key. The dimensions of the tensor are
-          'batch_size' x 'num_classes'.
-        """
-        lstm_size = FLAGS.lstm_cells
-        number_of_layers = FLAGS.lstm_layers
-
-        stacked_lstm = tf.contrib.rnn.MultiRNNCell(
+    stacked_lstm = tf.contrib.rnn.MultiRNNCell(
             [
                 tf.contrib.rnn.BasicLSTMCell(
-                    lstm_size, forget_bias=1.0, state_is_tuple=False)
+                    lstm_size, forget_bias=1.0)
                 for _ in range(number_of_layers)
-            ], state_is_tuple=False)
+                ])
 
-        loss = 0.0
+    loss = 0.0
 
-        outputs, state = tf.nn.dynamic_rnn(stacked_lstm, model_input,
-                                           sequence_length=num_frames,
-                                           dtype=tf.float32)
+    outputs, state = tf.nn.dynamic_rnn(stacked_lstm, model_input,
+                                       sequence_length=num_frames,
+                                       dtype=tf.float32)
 
-        aggregated_model = getattr(video_level_models,
-                                   FLAGS.video_level_classifier_model)
+    aggregated_model = getattr(video_level_models,
+                               FLAGS.video_level_classifier_model)
 
-        return aggregated_model().create_model(
-            model_input=state,
-            vocab_size=vocab_size,
-            **unused_params)
+    return aggregated_model().create_model(
+        model_input=state[-1].h,
+        vocab_size=vocab_size,
+        **unused_params)
 
 
 class BiLstmModel(models.BaseModel):
@@ -271,7 +269,6 @@ class BiLstmModel(models.BaseModel):
                 tf.contrib.rnn.BasicLSTMCell(
                     lstm_size, forget_bias=1.0)
                 for _ in range(number_of_layers)
-<<<<<<< HEAD
             ], state_is_tuple=False)
 
         #lstm_fw = tf.contrib.rnn.BasicLSTMCell(lstm_size, forget_bias=1.0, state_is_tuple=False)
@@ -279,9 +276,6 @@ class BiLstmModel(models.BaseModel):
         #lstm_bw = tf.contrib.rnn.BasicLSTMCell(lstm_size, forget_bias=1.0, state_is_tuple=False)
 
         loss = 0.0
-=======
-                ])
->>>>>>> refs/remotes/google/master
 
         outputs, state = tf.nn.bidirectional_dynamic_rnn(
             stacked_lstm_fw,
@@ -298,14 +292,7 @@ class BiLstmModel(models.BaseModel):
         # state = tf.concat(2, state)
         state = state[0] + state[1]
 
-<<<<<<< HEAD
-        return aggregated_model().create_model(
-            model_input=state,
-            vocab_size=vocab_size,
-            **unused_params)
-=======
     return aggregated_model().create_model(
         model_input=state[-1].h,
         vocab_size=vocab_size,
         **unused_params)
->>>>>>> refs/remotes/google/master

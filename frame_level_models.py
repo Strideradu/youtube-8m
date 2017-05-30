8a9003040f597f7dd6b3ef092e8703661b334325
Strideradu <dunan00001@hotmail.com>
2017-05-30 15:18:26 -0400

    change reshape to squeeze

--- frame_level_models.py
+++ frame_level_models.py
@@ -829,7 +829,8 @@ class SeqCNNLstmModel(models.BaseModel):
 
         num_filters_total = num_filters * len(filter_sizes)
         h_pool = tf.concat(pooled_outputs, 3)
-        new_model_input = tf.reshape(h_pool, [-1, new_frames, num_filters_total])
+        new_model_input = tf.squeeze(h_pool)
+        # new_model_input = tf.reshape(h_pool, [-1, new_frames, num_filters_total])
         print(new_model_input.get_shape().as_list()[1])
         print(new_model_input.get_shape().as_list()[2])
 
